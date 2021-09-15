affb8a5eee8c4cd0d379c1fa2743e76fd02771d0
vijayjayakumar02 <76395344+vijayjayakumar02@users.noreply.github.com>
2021-09-15 10:37:26 +0530

    Update MenuRepository.cs

--- RestaurantApp/RestaurantApp.DataAccess/Repository/MenuRepository.cs
+++ RestaurantApp/RestaurantApp.DataAccess/Repository/MenuRepository.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TastyDots.DataAccess.Data;
-using TastyDots.Models;
+using RestaurantApp.DataAccess.Data;
+using RestaurantApp.Models;
 
-namespace TastyDots.DataAccess.Repository
+namespace RestaurantApp.DataAccess.Repository
 {
     public class MenuRepository : IMenuRepository
     {
